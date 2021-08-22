415811c2cb761bb9007d69004131558eb468c3bb
delaluz12 <75863526+delaluz12@users.noreply.github.com>
2021-08-22 01:38:57 -0500

    finished all functions needed to get HTML string to return

--- index.js
+++ index.js
@@ -1,77 +1,152 @@
 //packages needed for application
-
 const inquirer = require('inquirer');
 const fs = require('fs');
-const employee = require('./lib/employee');
-const manager = require('./lib/manager');
-const engineer = require('./lib/engineer');
-const intern = require('./lib/intern');
+const Employee = require('./lib/employee');
+const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+const generateHTML = require('./src/page-template');
 
-//inquirer questions to gather data about team
-const questions = [
-    {
-        type: 'input',
-        name: 'managerName',
-        message: "What is the team manager's name?"
-    },
-    {
-        type: 'input',
-        name: 'managerId',
-        message: "What is the team manager's ID?"
-    },
-    {
-        type: 'input',
-        name: 'managerEmail',
-        message: "What is the team manager's email?"
-    },
-    {
-        type: 'input',
-        name: 'managerOffice',
-        message: "What is the team manager's office number?"
-    },
-    {
-        type: 'list',
-        name: 'teamMemberType',
-        message: "What type of team member would you like to add?",
-        choices: ['Engineer', 'Intern', "I don't want to add any more team members"],
-    },
-    {
-        type: 'input',
-        name: 'engineerName',
-        message: "What is the engineer's name?",
-        when:(responseObj) => responseObj.teamMemberType === 'Engineer',
-    },
-    {
-        type: 'input',
-        name: 'engineerEmail',
-        message: "What is the engineer's email?",
-    },
-    {
-        type: 'input',
-        name: 'engineerGithub',
-        message: "What is the engineer's GitHub username?"
-    },
-    {
-        type: 'input',
-        name: 'internName',
-        message: "What is the intern's name?",
-        when:(responseObj) => responseObj.teamMemberType === 'Intern'
-    },
-    {
-        type: 'input',
-        name: 'internId',
-        message: "What is the intern's ID?"
-    },
-    {
-        type: 'input',
-        name: 'internSchool',
-        message: "What school does the intern attend?"
-    },
+//path where output will be printed
+const path = require('path');
+const pathDirectory = path.resolve(__dirname, 'dist'); //pathDirectory 
+const outputPath = path.join(pathDirectory, 'team.html',)
 
+//array to store team member objects from constructor
+const teamArr = [];
 
-];
+//function that initates the application
 function init() {
     console.log("Let's start building your team")
-    inquirer.prompt({questions});
-    
+    //function that creates manager
+    function generateManager() {
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'managerName',
+                message: "What is the team manager's name?"
+            },
+            {
+                type: 'input',
+                name: 'managerId',
+                message: "What is the team manager's ID?"
+            },
+            {
+                type: 'input',
+                name: 'managerEmail',
+                message: "What is the team manager's email?"
+            },
+            {
+                type: 'input',
+                name: 'managerOffice',
+                message: "What is the team manager's office number?"
+            },
+        ]).then(response => {
+            const manager = new Manager(response.managerName, response.managerId, response.managerEmail, response.managerOffice);
+            //push manager to array of teamMembers
+            teamArr.push(manager);
+            //function that will build out team members under manager entered
+            generateTeamMembers();
+        })
+    };
+
+    //function that will build out team members under manager entered
+    function generateTeamMembers() {
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'teamMemberType',
+                message: "What type of team member would you like to add?",
+                choices: ['Engineer', 'Intern', "I don't want to add any more team members"],
+            },
+        ]).then(managerChoice => {
+            //switch case for the 3 diff case types
+            switch (managerChoice.teamMemberType) {
+                case 'Engineer':
+                    addEngineer();
+                    break;
+                case 'Intern':
+                    addIntern();
+                    break;
+                default:
+                    generateTeam(); 
+            }
+        });
+    };
+
+    function addEngineer() {
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'engineerName',
+                message: "What is the engineer's name?",
+            },
+            {
+                type: 'input',
+                name: 'engineerEmail',
+                message: "What is the engineer's email?",
+            },
+            {
+                type: 'input',
+                name: 'engineerId',
+                message: "What is the engineer's ID?"
+            },
+            {
+                type: 'input',
+                name: 'engineerGithub',
+                message: "What is the engineer's GitHub username?"
+            },
+        ]).then(response => {
+            const engineer = new Engineer(response.engineerName, response.engineerId, response.engineerEmail, response.engineerGithub);
+            teamArr.push(engineer);
+            generateTeamMembers(); //call this function again to ask if want to add more team members
+        });
+    };
+
+    function addIntern() {
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'internName',
+                message: "What is the intern's name?",
+            },
+            {
+                type: 'input',
+                name: 'internId',
+                message: "What is the intern's ID?"
+            },
+            {
+                type: 'input',
+                name: 'internEmail',
+                message: "What is the intern's email?"
+            },
+            {
+                type: 'input',
+                name: 'internSchool',
+                message: "What school does the intern attend?"
+            },
+        ]).then(response => {
+            const intern = new Intern(response.internName, response.internId, response.internEmail, response.internSchool);
+            teamArr.push(intern);
+            generateTeamMembers();
+        });
+    };
+    //function that generates entire team to be rendered on HTML page
+    function generateTeam() {
+        console.log(generateHTML(teamArr));
+        //check to see if pathDirectory already exists if not then create it
+        // if (!pathDirectory) {
+        //     fs.mkdirSync(pathDirectory);
+        // }
+        //write data to html file and generate team.html file in dist folder
+        // fs.writeFile(outputPath, generateHTML(teamArr), (err)=> {
+        //     err ? console.log(err) : console.log("success your team.html has rendered");
+        // })
+    };
+
+    //call manager function
+    generateManager();
 }
+
+//start app
+init();
--- src/page-template.js
+++ src/page-template.js
@@ -1,19 +1,96 @@
-function generateHTML(dataObj) {
+const Employee = require("../lib/employee");
+
+function generateHTML(teamArr) {
+    // console.log(teamArr);
+
+    const renderManagerCard = (manager) => {
+        return `<div class="col">
+    <div class="card">
+        <div class="cardHeaderWrapper white-text">
+            <div class="cardHeader">${manager.getName()}</div>
+            <div class="cardHeader"><i class="material-icons small">manage_accounts</i> ${manager.getRole()}
+            </div>
+        </div>
+        <div class="card-content grey lighten-5 black-text">
+            <ul class="collection">
+                <li class="collection-item">ID: <span id="managerID"> ${manager.getId()}</span></li>
+                <li class="collection-item">Email: <a href="mailto:${manager.getEmail()}" id="managerEmail"> ${manager.getEmail()}</a>
+                </li>
+                <li class="collection-item">Office Number: <span id="officeNum"> ${manager.getOfficeNum()}</span>
+                </li>
+            </ul>
+        </div>
+    </div>
+        </div>`
+    }
+
+    const renderEngineerCard = (engineer) => {
+        return `<div class="col">
+        <div class="card">
+            <div class="cardHeaderWrapper white-text">
+                <div class="cardHeader">${engineer.getName()}</div>
+                <div class="cardHeader"><i class="material-icons small">engineering</i> ${engineer.getRole()}
+                </div>
+            </div>
+            <div class="card-content grey lighten-5 black-text">
+                <ul class="collection">
+                    <li class="collection-item">ID: <span id="engineerID">${engineer.getId()}</span></li>
+                    <li class="collection-item">Email: <a href="mailto:${engineer.getEmail()}" id="engineerEmail"> ${engineer.getEmail()}</a>
+                    </li>
+                    <li class="collection-item">GitHub: <a href="https://github.com/${engineer.getUsername()}" id="engineerGithub">
+                    ${engineer.getUsername()}</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>`
+    }
+
+    const renderInternCard = (intern) => {
+        return `<div class="col">
+        <div class="card">
+            <div class="cardHeaderWrapper white-text">
+                <div class="cardHeader">${intern.getName()}</div>
+                <div class="cardHeader"><i class="material-icons small">school</i> ${intern.getRole()}
+                </div>
+            </div>
+            <div class="card-content grey lighten-5 black-text">
+                <ul class="collection">
+                    <li class="collection-item">ID: <span id="internID">${intern.getId()}</span></li>
+                    <li class="collection-item">Email: <a href="mailto:${intern.getEmail()}" id="internEmail"> ${intern.getEmail()}</a>
+                    </li>
+                    <li class="collection-item">School: <span id="internGithub"> ${intern.getSchool()}</span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>`
+    }
+
+    const manager = teamArr[0];
+    // console.log(renderManagerCard(manager));
+
+    //filter through teamArr for employee type then map through them to run appropriate function to generate card for each type of employee
+    const engineerHTML = teamArr.filter((employee)=> employee.getRole() === "Engineer").map((engineer)=> renderEngineerCard(engineer)).join('');
+    // console.log(engineerHTML);
+
+    const internHTML = teamArr.filter((employee)=> employee.getRole() === "Intern").map((intern)=> renderInternCard(intern)).join('');
+    console.log(internHTML);
 
 
     return `<!DOCTYPE html>
     <html lang="en">
-    
+
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Team Profiles</title>
-    
+
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
         <link rel="stylesheet" href="../dist/style.css">
-    
+
     </head>
     <body>
     <nav>
@@ -21,10 +98,20 @@ function generateHTML(dataObj) {
         <a href="#" class="brand-logo center">My Team Profiles</a>
     </div>
     </nav>
-
+    <body>
+     <div class = "container">
+            <div class="row">
+            ${renderManagerCard(manager)}
+            ${engineerHTML}
+            ${internHTML}
+            </div>
+     </div>
+    </body>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     </body>
     </html>
     `
 }
+
+module.exports = generateHTML;
